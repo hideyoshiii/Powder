ceba5ef6308c8ba13f8b94a3845d816fa65cc840
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2017-11-21 13:28:27 +0900

    fix05

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,:sex,:age,:image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:sex,:age])
   end
 end
