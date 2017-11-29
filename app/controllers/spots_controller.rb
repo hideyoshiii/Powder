2126bf9fab740ef7960a473b17ba33c70c708b1a
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2017-11-30 02:18:38 +0900

    fix43

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -19,9 +19,9 @@ class SpotsController < ApplicationController
     @spot = current_user.spots.build(spot_params)
 
     if @spot.save
-      redirect_to user_path(current_user), notice: "リスティングを作成・保存をしました"
+      redirect_to user_path(current_user)
     else
-      redirect_to new_spot_path, notice: "リスティングを作成・保存出来ませんでした"
+      redirect_to new_spot_path
     end
   end
 
@@ -29,14 +29,14 @@ class SpotsController < ApplicationController
     @scenes = ["クリスマス", "記念日", "サプライズ", "初デート", "お家デート", "アウトドア", "雨の日", "ショッピング", "癒し", "夜景", "テーマパーク", "美術館", "フォトジェニック", "朝食", "ランチ", "ディナー", "カフェ", "バー", "居酒屋", "スイーツ", "食べ歩き", "春", "夏", "秋", "冬"]
     @spot = Spot.find(params[:id])
     if !(current_user == @spot.user)
-      redirect_to root_path, notice: "他人の編集ページにはアクセスできません"
+      redirect_to root_path
     end
   end
 
   def update
     @spot = Spot.find(params[:id])
     if @spot.update(spot_params)
-      redirect_to user_path(current_user), notice: "更新しました"
+      redirect_to user_path(current_user)
     end
   end
 
