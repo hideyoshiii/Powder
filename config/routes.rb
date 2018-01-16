7293a3c04bff6a0ec9defec834e9eb2a75c98651
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-01-16 19:26:49 +0900

    fix200

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   get "articles/2" => "articles#show2"
   get "articles/3" => "articles#show3"
   get "articles/4" => "articles#show4"
+  get "articles/5" => "articles#show5"
 
 
   get "questions/men" => "questions#men"
