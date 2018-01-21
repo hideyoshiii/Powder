d439e176c2698cab7c62c6805708316f016fcbcd
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-01-21 14:40:32 +0900

    fix239

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,27 +6,39 @@ class CoursesController < ApplicationController
 
   def result
   	    @spot1 = Spot.where(city: params[:city], large: "レストラン", price: params[:price]).order("RANDOM()").first
-  	    @pictures1 = @spot1.pictures
+  	    if @spot1
+  	    	@pictures1 = @spot1.pictures
+  	    end
 
     	if params[:large].to_i == 1
     		@spot2 = Spot.where(city: params[:city], large: "バー").order("RANDOM()").first
-    		@pictures2 = @spot2.pictures
+    		if @spot2
+    			@pictures2 = @spot2.pictures
+    		end
     	end
     	if params[:large].to_i == 2
     		@spot2 = Spot.where(city: params[:city], large: "カフェ").order("RANDOM()").first
-    		@pictures2 = @spot2.pictures
+    		if @spot2
+    			@pictures2 = @spot2.pictures
+    		end
     	end
     	if params[:large].to_i == 3
     		@spot2 = Spot.where(city: params[:city], large: "アクティブ").order("RANDOM()").first
-    		@pictures2 = @spot2.pictures
+    		if @spot2
+    			@pictures2 = @spot2.pictures
+    		end
     	end
     	if params[:large].to_i == 4
     		@spot2 = Spot.where(city: params[:city], large: "夜景").order("RANDOM()").first
-    		@pictures2 = @spot2.pictures
+    		if @spot2
+    			@pictures2 = @spot2.pictures
+    		end
     	end
     	if params[:large].to_i == 5
-    		@spot2 = Spot.where(city: params[:city], large: "").order("RANDOM()").first
-    		@pictures2 = @spot2.pictures
+    		@spot2 = Spot.where(city: params[:city], large: "バー").or(Spot.where(city: params[:city], large: "カフェ")).order("RANDOM()").first
+    		if @spot2
+    			@pictures2 = @spot2.pictures
+    		end
     	end
   end
 
