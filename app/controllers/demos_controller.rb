a834b247f518c13a1f2c008f5d8ad95be218f1ee
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-01-27 04:06:02 +0900

    fix270

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -8,37 +8,31 @@ class DemosController < ApplicationController
   end
 
   def dinner
+  	@spots = Spot.where("scenes like '%ディナー%'")
+   	@spots = @spots.where(city: params[:city], price: params[:price]).order("RANDOM()").limit(3)
   end
 
   def search2
+  	if params[:large] == "２軒目なし"
+  		render :action => "result"
+  	else
+  		render :action => "second"
+  	end
   end
 
   def second
+  	@scene = params[:large]
+  	@spot1 = Spot.find(params[:spot1])
+  	@spots = Spot.where.not(title: @spot1.title)
+  	@spots = @spots.where("scenes like '%#{@scene}%'")
+  	@spots = @spots.where(station: @spot1.station).order("RANDOM()").limit(3)
   end
 
   def result
-        @spot11 = Spot.where("scenes like '%ディナー%'")
-  	    @spot1 = @spot11.where(city: params[:city], price: params[:price]).order("RANDOM()").first
-  	    if @spot1
-  	    	@pictures1 = @spot1.pictures.order(id: "ASC")
-          @spot21 = Spot.where.not(title: @spot1.title)
-
-        	if params[:large].to_i == 1
-            @spot22 = @spot21.where("scenes like '%バー%'")
-        		@spot2 = @spot22.where(station: @spot1.station).order("RANDOM()").first
-        		if @spot2
-        			@pictures2 = @spot2.pictures.order(id: "ASC")
-        		end
-        	end
-        	if params[:large].to_i == 2
-            @spot22 = @spot21.where("scenes like '%カフェ%'")
-            @spot2 = @spot22.where(station: @spot1.station).order("RANDOM()").first
-        		if @spot2
-        			@pictures2 = @spot2.pictures.order(id: "ASC")
-        		end
-        	end
-
-        end
+    @spot1 = Spot.find(params[:spot1])
+    if params[:spot2]
+    	@spot2 = Spot.find(params[:spot2])
+    end
   end
 
   
