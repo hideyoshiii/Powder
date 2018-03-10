41f02cd4749f25feb1e5d000a0ca0b625de686a5
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-03-10 18:59:32 +0900

    fix679

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -5,11 +5,3 @@ if ENV['RACK_ENV'] == 'production'
     }
   end
 end
-
-if ENV['RACK_ENV'] == 'production'
-  Powder::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{.*}, 'https://www.a-date.jp$&', :if => Proc.new {|rack_env|
-      rack_env['SERVER_NAME'] == 'http://www.a-date.jp'
-    }
-  end
-end
