0d2f2323a941897d01406a48cf5a96683f2b017f
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-04-22 17:12:04 +0900

    fix852

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,11 +3,8 @@ class Course < ApplicationRecord
   has_many :points, dependent: :destroy
   has_many :timelines, dependent: :destroy
   has_many :copies, dependent: :destroy
-  has_many :loves, dependent: :destroy
+  has_many :love, dependent: :destroy
 
   acts_as_taggable_on :course # course.course_list が追加される
 
-  def love_user(user_id)
-   loves.find_by(user_id: user_id)
-  end
 end
