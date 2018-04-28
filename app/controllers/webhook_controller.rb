413b917eaf3bf535cd33e09ecd201da3ede6f920
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-04-28 21:42:10 +0900

    fix953

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -5,36 +5,53 @@ class WebhookController < ApplicationController
   protect_from_forgery :except => [:callback,:client]
 
   def client
-    @client ||= Line::Bot::Client.new { |config|
+    client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 
   def callback
-    body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-
-    events = client.parse_events_from(body)
-
-    events.each { |event|
-      case event
+ 
+  body = request.body.read
+ 
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+ 
+  event = params["events"][0]
+  event_type = event["type"]
+ 
+  #送られたテキストメッセージをinput_textに取得
+  input_text = event["message"]["text"]
+ 
+  events = client.parse_events_from(body)
+ 
+  events.each { |event|
+ 
+    case event
       when Line::Bot::Event::Message
         case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-      end
-    }
-
-    head :ok
-  end
+          #テキストメッセージが送られた場合、そのままおうむ返しする
+          when Line::Bot::Event::MessageType::Text
+             message = {
+                  type: 'text',
+                  text: input_text
+                  }
+ 
+          #画像が送られた場合、適当な画像を送り返す
+          #画像を返すには、画像が保存されたURLを指定する。
+          #なお、おうむ返しするには、１度AWSなど外部に保存する必要がある。ここでは割愛する
+          when Line::Bot::Event::MessageType::Image
+            image_url = "https://XXXXXXXXXX/XXX.jpg"  #httpsであること
+              message = {
+                  type: "image",
+                  originalContentUrl: image_url,
+                  previewImageUrl: image_url
+                  }
+         end #event.type
+         #メッセージを返す
+         client.reply_message(event['replyToken'],message)
+    end #event
+ } #events.each
+ 
+end  #def
 end
