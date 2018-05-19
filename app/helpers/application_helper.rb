489607f5c2ca79adc7709e8e3717f3d9cfe64477
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-05-19 09:37:28 +0900

    fix1130

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 	def default_meta_tags
     {
       site: 'A.Date(アデート)',
-      title: '誰でもデートコースを10秒で',
+      title: 'デートコースを10秒で',
       reverse: true,
       charset: 'utf-8',
       description: 'A.Date(アデート)はデートコースを10秒でつくれるサービスです。WebもしくはLINEで質問に答えていくだけでAIにデートコースを提案してもらうことができます。',
-      keywords: 'デートコース, 東京, 提案, 10秒',
+      keywords: 'デート, 東京, 提案',
       canonical: 'https://www.a-date.jp',
       separator: '|',
       icon: [
@@ -16,7 +16,7 @@ module ApplicationHelper
       og: {
         site_name: 'A.Date(アデート)',
         title: 'A.Date(アデート)',
-        description: '誰でもデートコースを10秒で',
+        description: 'デートコースを10秒で',
         type: 'website',
         url: 'https://www.a-date.jp',
         image: image_url('title.jpg'),
@@ -26,7 +26,7 @@ module ApplicationHelper
         card: 'summary_large_image',
         site: '@adate_tw',
         title: 'A.Date(アデート)',
-        description: '誰でもデートコースを10秒で',
+        description: 'デートコースを10秒で',
         type: 'website',
         url: 'https://www.a-date.jp',
         image: image_url('title.jpg')
